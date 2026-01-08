8ba4f7b67fbaa66293fe09d38a05845a7f2e70ba
Mbarek Benraiss <81750067+malika-1989@users.noreply.github.com>
2026-01-08 07:53:08 +0100

    add two polynomials

--- Matrices/polynomial/Poynomial.c
+++ Matrices/polynomial/Poynomial.c
@@ -38,6 +38,47 @@ void display(struct Polynomial p)
     printf("\n");
 }
 
+struct Polynomial *add(struct Polynomial *p1, struct Polynomial *p2)
+{
+    /* data */
+    int i, j, k;
+    struct Polynomial *sum;
+
+    sum = malloc(sizeof(struct Polynomial));
+    sum->terms = malloc((p1->n + p2->n) * sizeof(struct Term));
+    sum->n = p1->n + p2->n;
+
+    i = j = k = 0;
+
+    // this loop in to compare exponents of both polynomials and add accordingly
+    while (i < p1->n && j < p2->n)
+    {
+        if (p1->terms[i].exp > p2->terms[j].exp)
+        {
+            sum->terms[k++] = p1->terms[i++];
+        }
+        else if (p1->terms[i].exp < p2->terms[j].exp)
+        {
+            sum->terms[k++] = p2->terms[j++];
+        }
+        else
+        {
+            sum->terms[k].exp = p1->terms[i].exp;
+            sum->terms[k++].coeff = p1->terms[i++].coeff + p2->terms[j++].coeff;
+        }
+    }
+
+    for (; i < p1->n; i++)
+    {
+        sum->terms[k++] = p1->terms[i];
+    }
+    for (; j < p2->n; j++)
+    {
+        sum->terms[k++] = p2->terms[j];
+    }
+    return sum;
+};
+
 void cleanup(struct Polynomial *p)
 {
     free(p->terms);
@@ -47,7 +88,22 @@ void cleanup(struct Polynomial *p)
 
 int main()
 {
-    struct Polynomial p1;
+    struct Polynomial p1, p2, *p3;
+
+    create(&p1);
+    create(&p2);
+    p3 = add(&p1, &p2);
+
+    printf("\nPolynomial 1: ");
+    display(p1);
+    printf("Polynomial 2: ");
+    display(p2);
+    printf("Sum: ");
+    display(*p3);
+
+    cleanup(&p1);
+    cleanup(&p2);
+    cleanup(p3);
     create(&p1);
 
     printf("\nPolynomial: ");
@@ -58,17 +114,39 @@ int main()
 }
 
 //*═══════════════════════════════════════════════════════
-//DEMO COMPLETE
-//═══════════════════════════════════════════════════════* /
-    /**
-     *
-     *
-     * Number of terms: 4
-    Enter all terms (coeff exp) in decreasing order of exponents:
-    Term 1: 2 3
-    Term 2: 5 2
-    Term 3: 3 1
-    Term 4: 4 0
+// DEMO COMPLETE code polynomial version C
+// ═══════════════════════════════════════════════════════* /
+/**
+ *
+ *
+ benraiss@MacBookAir Matrices % clang -std=c17 -Wall -Wextra -o poynomial_cpp ./polynomial/Poynomial.c
+benraiss@MacBookAir Matrices % ./poynomial_cpp
+// add two polynomials :
 
-    Polynomial: 2x^3 + 5x^2 + 3x^1 + 4x^0
-     */
+Number of terms: 3
+Enter all terms (coeff exp) in decreasing order of exponents:
+Term 1: 1 5
+Term 2: 1 3
+Term 3: 1 1
+Number of terms: 3
+Enter all terms (coeff exp) in decreasing order of exponents:
+Term 1: 1 4
+Term 2: 1 2
+Term 3: 1 0
+
+Polynomial 1: 1x^5 + 1x^3 + 1x^1
+Polynomial 2: 1x^4 + 1x^2 + 1x^0
+Sum: 1x^5 + 1x^4 + 1x
+
+ *
+ *
+ *
+ * Number of terms: 4
+Enter all terms (coeff exp) in decreasing order of exponents:
+Term 1: 2 3
+Term 2: 5 2
+Term 3: 3 1
+Term 4: 4 0
+
+Polynomial: 2x^3 + 5x^2 + 3x^1 + 4x^0
+ */
