f0e25e484adea137b74d5b9f57cb0857c216e598
Mbarek Benraiss <81750067+malika-1989@users.noreply.github.com>
2026-01-09 09:13:25 +0100

    LL  min max

--- linkedlist/LLDisplay.c
+++ linkedlist/LLDisplay.c
@@ -5,6 +5,8 @@ struct Node
 {
     int data;
     struct Node *next;
+    struct Node *prev;
+
 } *first;
 
 void create(int A[], int n)
@@ -91,9 +93,83 @@ int RSum(struct Node *p)
     }
 }
 
+int Max(struct Node *p)
+{
+    int max = INT32_MIN;
+
+    while (p != NULL)
+    {
+        if (p->data > max)
+        {
+            max = p->data;
+        }
+        p = p->next;
+    }
+    return max;
+}
+
+int RMax(struct Node *p)
+{
+    int x = 0;
+
+    if (p == 0)
+    {
+        return INT32_MIN;
+    }
+    x = RMax(p->next);
+
+    if (x > p->data)
+    {
+        return x;
+    }
+    else
+    {
+        return p->data;
+    }
+    // Alternatively, using the conditional operator:
+    // return (x > p->data) ? x : p->data;
+}
+
+int Min(struct Node *p)
+{
+    int min = INT32_MAX;
+
+    while (p != NULL)
+    {
+        if (p->data < min)
+        {
+            min = p->data;
+        }
+        p = p->next;
+    }
+    return min;
+}
+
+int RMin(struct Node *p)
+{
+    int x = 0;
+
+    if (p == 0)
+    {
+        return INT32_MAX;
+    }
+    x = RMin(p->next);
+
+    if (x < p->data)
+    {
+        return x;
+    }
+    else
+    {
+        return p->data;
+    }
+    // Alternatively, using the conditional operator:
+    // return (x < p->data) ? x : p->data;
+}
+
 int main()
 {
-    int A[] = {3, 5, 7, 10, 15};
+    int A[] = {3, 5, 7, 10, 30};
     create(A, 5);
     printf("Linked List Elements: ");
     display(first);
@@ -104,18 +180,25 @@ int main()
     printf("Recursive Count: %d\n", RCount(first));
     printf("Sum of elements in Linked List: %d\n", sum(first));
     printf("Recursive Sum: %d\n", RSum(first));
+    printf("Maximum element in Linked List: %d\n", Max(first));
+    printf("Recursive Maximum: %d\n", RMax(first));
+    printf("Minimum element in Linked List: %d\n", Min(first));
+    printf("Recursive Minimum: %d\n", RMin(first));
 
     return 0;
 }
 // Output:
 /*
-    benraiss@Mbareks-MacBook-Air linkedlist % clang -std=c17 -Wall -Wextra -o ll_display LLDisplay.c
+  benraiss@Mbareks-MacBook-Air linkedlist % clang -std=c17 -Wall -Wextra -o ll_display LLDisplay.c
     benraiss@Mbareks-MacBook-Air linkedlist % ./ll_display
-    Linked List Elements: 3 5 7 10 15
-    Recursive Display: 3 5 7 10 15
+    Linked List Elements: 3 5 7 10 30
+    Recursive Display: 3 5 7 10 30
     Number of elements in Linked List: 5
     Recursive Count: 5
-    Sum of elements in Linked List: 40
-    Recursive Sum: 40
-    benraiss@Mbareks-MacBook-Air linkedlist %
+    Sum of elements in Linked List: 55
+    Recursive Sum: 55
+    Maximum element in Linked List: 30 //max iterative version works too
+    Recursive Maximum: 30 // max recursive version works too
+    Minimum element in Linked List: 3 // min iterative version
+    Recursive Minimum: 3 // min recursive version
   */
