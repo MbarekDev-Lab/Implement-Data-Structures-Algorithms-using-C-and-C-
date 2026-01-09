266d98c3949b4ed730336cc32f4402bbedea9f2f
Mbarek Benraiss <81750067+malika-1989@users.noreply.github.com>
2026-01-09 08:40:20 +0100

    code Linkrdlist in c RCount + RSum Ele

--- linkedlist/LLDisplay.c
+++ linkedlist/LLDisplay.c
@@ -41,13 +41,58 @@ void RDisplay(struct Node *p)
     if (p != NULL)
     {
         printf("%d ", p->data);
-        RDDisplay(p->next);
+        RDisplay(p->next);
+    }
+}
+
+int count(struct Node *p)
+{
+    int l = 0;
+    while (p != NULL)
+    {
+        l++;
+        p = p->next;
+    }
+    return l;
+}
+
+int RCount(struct Node *p)
+{
+    if (p != NULL)
+    {
+        return RCount(p->next) + 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int sum(struct Node *p)
+{
+    int s = 0;
+    while (p != NULL)
+    {
+        s += p->data;
+        p = p->next;
+    }
+    return s;
+}
+
+int RSum(struct Node *p)
+{
+    if (p == NULL)
+    {
+        return 0;
+    }
+    else
+    {
+        return RSum(p->next) + p->data;
     }
 }
 
 int main()
 {
-
     int A[] = {3, 5, 7, 10, 15};
     create(A, 5);
     printf("Linked List Elements: ");
@@ -55,6 +100,22 @@ int main()
     printf("Recursive Display: ");
     RDisplay(first);
     printf("\n");
-    
+    printf("Number of elements in Linked List: %d\n", count(first));
+    printf("Recursive Count: %d\n", RCount(first));
+    printf("Sum of elements in Linked List: %d\n", sum(first));
+    printf("Recursive Sum: %d\n", RSum(first));
+
     return 0;
 }
+// Output:
+/*
+    benraiss@Mbareks-MacBook-Air linkedlist % clang -std=c17 -Wall -Wextra -o ll_display LLDisplay.c
+    benraiss@Mbareks-MacBook-Air linkedlist % ./ll_display
+    Linked List Elements: 3 5 7 10 15
+    Recursive Display: 3 5 7 10 15
+    Number of elements in Linked List: 5
+    Recursive Count: 5
+    Sum of elements in Linked List: 40
+    Recursive Sum: 40
+    benraiss@Mbareks-MacBook-Air linkedlist %
+  */
