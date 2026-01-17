d577fc5116219157f7a2bc3a947238f30f968083
Mbarek Benraiss <81750067+malika-1989@users.noreply.github.com>
2026-01-17 20:23:23 +0100

    pop peek stackTop

--- stack/Stack.c
+++ stack/Stack.c
@@ -9,6 +9,7 @@ struct Stack
     int *S;
 };
 
+// creating stack
 void CreateStack(struct Stack *st)
 {
     printf("Enter size of stack: ");
@@ -17,17 +18,7 @@ void CreateStack(struct Stack *st)
     st->S = (int *)malloc(st->size * sizeof(int));
 }
 
-// copying the structure is NOT a problem here because the structure contains a pointer to dynamically allocated memory
-void Display(struct Stack st)
-{
-    int i;
-    for (i = st.top; i >= 0; i--)
-    {
-        printf("%d ", st.S[i]);
-    }
-    printf("\n");
-}
-
+// push operation on stack
 void Push(struct Stack *st, int x)
 {
     if (st->top == st->size - 1)
@@ -39,6 +30,65 @@ void Push(struct Stack *st, int x)
         st->top++;
         st->S[st->top] = x;
     }
+}
+
+// pop operation on stack
+int pop(struct Stack *st)
+{
+    int x = -1;
+
+    if (st->top == -1)
+        printf("Stack Underflow\n");
+    else
+    {
+        x = st->S[st->top--];
+    }
+    return x;
+}
+
+// peek operation on stack
+int peek(struct Stack st, int index)
+{
+    int x = -1;
+    if (st.top - index + 1 < 0)
+        printf("Invalid Index \n");
+    x = st.S[st.top - index + 1];
+
+    return x;
+}
+
+// check if stack is empty
+int isEmpty(struct Stack st)
+{
+    if (st.top == -1)
+        return 1;
+    return 0;
+}
+
+// check if stack is full
+int isFull(struct Stack st)
+{
+    return st.top == st.size - 1;
+}
+
+// get the top element of the stack
+int stackTop(struct Stack st)
+{
+    if (!isEmpty(st))
+        return st.S[st.top];
+    return -1;
+}
+
+// copying the structure is NOT a problem here because the structure
+// contains a pointer to dynamically allocated memory
+void Display(struct Stack st)
+{
+    int i;
+    for (i = st.top; i >= 0; i--)
+    {
+        printf("%d ", st.S[i]);
+    }
+    printf("\n");
 }
 
 int main()
@@ -57,6 +107,9 @@ int main()
     Push(&st, 40);
     Push(&st, 50);
 
+    printf("%d \n", stackTop(st)); // print top element
+    printf("%d \n", peek(st, 1));  // print element at index 1
+
     printf("Stack after pushing 10, 20, 30, 40, 50:\n");
     Display(st);
 
@@ -68,18 +121,17 @@ int main()
     return 0;
 }
 
-/*benraiss@MacBookAir stack % clang -std=c17 -Wall -Wextra -o stack_ll Stack.c
-benraiss@MacBookAir stack % ./stack_ll
-═══════════════════════════════════════════════════════
+/*═══════════════════════════════════════════════════════
             ABDUL BARI'S STACK IMPLEMENTATION
 ═══════════════════════════════════════════════════════
 
 Enter size of stack: 5
 
 ═══ PUSH OPERATIONS ═══
+50
+50
 Stack after pushing 10, 20, 30, 40, 50:
 50 40 30 20 10
 
 ═══════════════════════════════════════════════════════
-Program completed successfully!
-benraiss@MacBookAir stack % */
+Program completed successfully!*/
